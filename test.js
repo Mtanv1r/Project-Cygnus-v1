89b56d3008a64bbe71fc76e9d236fe0e467c8086
tanvir <oxyzen2416256@gmail.com>
2026-08-15 20:58:24 +0600

    object flow visualization

---
+++ test.js
@@ -0,0 +1,60 @@
+//fake post collector 
+//evrery post will be an object 
+//array of 5 fake post object
+//filtering news  
+let postArr=[
+    {
+        text:'fifa world cup final ',
+        date:'13/8/2026',
+        url:'postLink',
+    },
+    {
+        text:'Ncp vice pesident el',
+        date:'15/8/2026',
+        url:'postLink',
+    },
+    {
+        text:'president speech about current destruction ',
+        date:'13/8/2026',
+        url:'postLink',
+    },
+     {
+        text:'jamal patwary speech  ',
+        date:'18/8/2026',
+        url:'postLink',
+    },
+         {
+        text:'Bnp rajniti',
+        date:'14/8/2026',
+        url:'postLink',
+    }
+];
+//filtering by date
+// const getPostByDate=(posts,date)=>{
+//     // filtering 
+//     newarr=posts.filter((elm)=>{
+//         if(elm.date===date){
+//             return elm;
+//         }
+//     });
+//     return newarr;
+// }
+//filtering by keyword
+// let getPostbyKeyword=(posts,keyword)=>{
+//     keyArr=posts.filter((elm)=>{
+//         if(elm.text.includes(keyword)){
+//             return elm;
+//         }
+//     });
+//     return keyArr;
+// }
+// console.log(getPostbyKeyword(postArr,'jamal'));
+let getPost=(posts , date , keyword)=>{
+    let newArr=posts.filter((elm)=>{
+        if(elm.date===date && elm.text.includes(keyword)){
+            return elm;
+        }
+    })
+    return newArr;
+}
+console.log(getPost(postArr,'18/8/2026','jamal'));
